1d52b5c0ff6766135fa24f56e9a7fe616d5a0d60
akarapon.ty <akarapon.ty@hotmail.com>
2021-02-15 14:29:48 +0700

    config cuda

--- KMUTTArchivesManagement/index/views.py
+++ KMUTTArchivesManagement/index/views.py
@@ -5,6 +5,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
+import tensorflow as tf
 
 from modelUser.views import UserController
 from modelDocument.views import DocumentController
@@ -102,15 +103,16 @@ def API_Deepcut(request):
         fulltext = data.get('fulltext')
         gpus = tf.config.list_physical_devices('GPU')
         if gpus:
-            # Restrict TensorFlow to only use the first GPU
             try:
-                tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-                logical_gpus = tf.config.experimental.list_logical_devices(
-                    'GPU')
-                print(len(gpus), "Physical GPUs,",
-                      len(logical_gpus), "Logical GPU")
+                # Currently, memory growth needs to be the same across GPUs
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+                    logical_gpus = tf.config.experimental.list_logical_devices(
+                        'GPU')
+                    print(len(gpus), "Physical GPUs,", len(
+                        logical_gpus), "Logical GPUs")
             except RuntimeError as e:
-                # Visible devices must be set before GPUs have been initialized
+                # Memory growth must be set before GPUs have been initialized
                 print(e)
         tokens = deepcut(fulltext)
 
--- KMUTTArchivesManagement/tokenizer/pipeLineToken.py
+++ KMUTTArchivesManagement/tokenizer/pipeLineToken.py
@@ -13,57 +13,27 @@ import concurrent.futures as cf
 from tqdm import tqdm
 from threading import Thread
 from tokenizer.read import *
-from tokenizer.deepcut import deepcut
 import tensorflow as tf
+from tokenizer.deepcut import deepcut
 
 
 def pipeLineTokenizer(filename, fulltext):
-    gpus = tf.config.list_physical_devices('GPU')
-    if gpus:
-        # Restrict TensorFlow to only use the first GPU
-        try:
-            tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,",
-                  len(logical_gpus), "Logical GPU")
-        except RuntimeError as e:
-            # Visible devices must be set before GPUs have been initialized
-            print(e)
-    # if(current_process().name == "SpawnPoolWorker-4"):
-    #     position = 0
-    # elif(current_process().name == "SpawnPoolWorker-5"):
-    #     position = 1
-    # elif(current_process().name == "SpawnPoolWorker-6"):
-    #     position = 2
-    # elif(current_process().name == "SpawnPoolWorker-7"):
-    #     position = 3
-    # elif(current_process().name == "SpawnPoolWorker-8"):
-    #     position = 4
-
-    # textDeepcut = '[SLOT:{position}]{filename}(Deepcut)'.format(
-    #     position=position,
-    #     filename=filename
-    # )
-    # textTokenize = '[SLOT:{position}]{filename}(Tokenize)'.format(
-    #     position=position,
-    #     filename=filename
-    # )
-    # textDone = '{filename}'.format(
-    #     filename=filename
-    # )
-
     corpusInPage = []
     resultCorpusInPage = []
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices(
+                    'GPU')
+                print(len(gpus), "Physical GPUs,", len(
+                    logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
 
-    # with tqdm(
-    #     position=position,
-    #     total=len(fulltext),
-    #     desc=textDeepcut,
-    #     ascii=True,
-    #     ncols=130,
-    #     leave=False,
-    #     disable=False,
-    # ) as pbarDeepcut:
     for line in fulltext:
         # Deep Cut process
         token = deepcut(line)
@@ -76,38 +46,14 @@ def pipeLineTokenizer(filename, fulltext):
         token = list(map(to_lower_case, token))
         # push to corpus
         corpusInPage.extend(token)
-        #     pbarDeepcut.update()
-        # pbarDeepcut.clear()
-
-    # # pyspell check
-    # corpusInPage = list(map(spellCheckAuto, corpusInPage))
-    # # Normalize
-    # corpusInPage = list(map(cleanDot, corpusInPage))
-    # # specific spellcheck
-    # corpusMED = loadSpecific()
-    # corpusInPage = list(
-    #     map(spellCheckSpecific, corpusInPage, repeat(corpusMED))
-    # )
 
     corpusMED = loadSpecific()
 
-    # with tqdm(
-    #     position=position,
-    #     total=len(corpusInPage),
-    #     desc=textTokenize,
-    #     ascii=True,
-    #     ncols=130,
-    #     leave=True,
-    #     disable=False,
-    # ) as pbarTokenize:
     for value in corpusInPage:
         word = spellCheckAuto(value)
         word = cleanDot(word)
         word = spellCheckSpecific(word, corpusMED)
         resultCorpusInPage.append(word)
-        # pbarTokenize.update()
-        # pbarTokenize.set_description(textDone)
-        # pbarTokenize.clear()
 
     pageIndex = 0
     regexSearch = re.search(r'(?<=page-)\d+(?=.txt)', filename)
